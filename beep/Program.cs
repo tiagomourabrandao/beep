7f6f534d6b9cdc005b5c850b6d266d625e0e6458
tiagomourabrandao <tiagomourabrandao@gmail.com>
2015-07-08 23:37:47 -0300

    First Commit

---
+++ beep/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace beep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Timer timer = new Timer();
+
+            timer.Elapsed += new ElapsedEventHandler(OnTimeEvent);
+
+            Console.WriteLine("Digite o tempo do seu timer em minutos");
+            var tempo = Console.ReadLine();
+
+            timer.Interval = int.Parse(tempo) * 10000;
+            timer.Enabled = true;
+
+            Console.WriteLine("Timer ajustado para " + tempo + " minutos.");
+            Console.WriteLine("Feito por Tiago Potência. http://about.me/tiagomourabrandao");
+            Console.ReadLine();
+
+        }
+
+        private static void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Console.Beep();
+        }
+    }
+}
